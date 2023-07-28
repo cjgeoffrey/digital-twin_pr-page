dc73a11c39aac8af759bebf631fbed7d5efebde2
Jerome Geoffrey <dearjaffy@icloud.com>
2023-07-28 13:41:34 +0300

    missing files

---
+++ src/data/peopleInConsortium.js
@@ -0,0 +1,72 @@
+export const peopleInConsortium = [
+  {
+    name: "Ilya Shmulevich (PI), Seattle",
+    link: "https://shmulevich.isbscience.org/",
+    linkDetail: "Lab Home Page",
+  },
+  {
+    name: "Boris Aguilar (Senior Research Scientist), Seattle",
+    link: "https://scholar.google.com/citations?user=z4vA4YAAAAAJ&hl=en&oi=ao",
+    linkDetail: "Google Scholar",
+  },
+  {
+    name: "David Gibbs (Senior Research Scientist), Seattle",
+    link: "https://scholar.google.com/citations?user=lMvBJ3IAAAAJ&hl=en&oi=ao",
+    linkDetail: "Google Scholar",
+  },
+  {
+    name: "Bahar Tercan ( Research Scientist), Seattle",
+    link: "https://scholar.google.com/citations?hl=en&user=BO5I1T0AAAAJ",
+    linkDetail: "Google Scholar",
+  },
+  {
+    name: "Vesteinn Thorsson (Principal Scientist), Seattle",
+    link: "https://scholar.google.com/citations?hl=en&user=2H_kMKsAAAAJ",
+    linkDetail: "Google Scholar",
+  },
+  {
+    name: "Guangrong Qin (Senior Research Scientist), Seattle",
+    link: "https://scholar.google.com/citations?hl=en&user=nravnEcAAAAJ",
+    linkDetail: "Google Scholar",
+  },
+  {
+    name: "Caroline Heckman (Co-PI), Helsinki",
+    link: "https://scholar.google.com/citations?hl=en&user=JO2HFYEAAAAJ",
+    linkDetail: "Google Scholar",
+  },
+  {
+    name: "Mika Kontro (Co-PI), Helsinki",
+    link: "https://scholar.google.com/citations?user=HMwJ3rYAAAAJ&hl=en&oi=ao",
+    linkDetail: "Google Scholar",
+  },
+  {
+    name: "Heikki Kuusanmäki (Postdoctoral Researcher), Helsinki",
+    link: "https://scholar.google.com/citations?hl=en&user=iZxajZgAAAAJ",
+    linkDetail: "Google Scholar",
+  },
+  {
+    name: "Philipp Sergeev (Doctoral Researcher), Helsinki",
+    link: "https://scholar.google.com/citations?hl=en&user=iZxajZgAAAAJ",
+    linkDetail: "Google Scholar",
+  },
+  {
+    name: "Olli Yli-Harja (Co-PI), Tampere",
+    link: "https://scholar.google.com/citations?user=HPcSSpUAAAAJ&hl=en&oi=ao",
+    linkDetail: "Google Scholar",
+  },
+  {
+    name: "Frank Emmert-Streib (Co-PI), Tampere",
+    link: "http://www.bio-complexity.com/index.html",
+    linkDetail: "Lab Home Page",
+  },
+  {
+    name: "Amer Farea (Postdoctoral Researcher), Tampere",
+    link: "",
+    linkDetail: "",
+  },
+  {
+    name: "Jerome Geoffrey Chandraseelan (Postdoctoral Researcher), Tampere",
+    link: "",
+    linkDetail: "",
+  },
+];
---
+++ src/data/publicationList.js
@@ -0,0 +1,14 @@
+export const publicationsList = [
+  {
+    title:
+      "Digital twins for predictive oncology will be a paradigm shift for precision cancer care",
+    link: "https://pubmed.ncbi.nlm.nih.gov/34824458/",
+    linkDetail: "To Page",
+  },
+  {
+    title:
+      "What Is a Digital Twin? Experimental Design for a Data-Centric Machine Learning Perspective in Health",
+    link: "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9653941/",
+    linkDetail: "To Page",
+  },
+];
